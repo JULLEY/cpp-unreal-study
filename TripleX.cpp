4a8860e6857580c5601e89716b37a70884ee10aa
woong <ung91@yahoo.com>
2022-03-20 02:08:09 +0900

    Variables, const & Assigning Values

--- TripleX.cpp
+++ TripleX.cpp
@@ -5,5 +5,17 @@ int main()
      std::cout << "Hello, World";
      std::cout << std::endl;
      std::cout << "End Game";
+
+     const int a = 111;
+     const int b = 222;
+     const int c = 333;
+
+     const int sum = a + b + c;
+     const int m = a * b * c;
+
+     std::cout << std::endl;
+     std::cout << sum << std::endl;
+     std::cout << m << std::endl;
+
      return 0;
  }
